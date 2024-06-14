6489b237e2c221cb77ebb3fe82106c138ad5b7d3
lcmartins <lcmartins@gmail.com>
2024-06-13 21:45:14 -0300

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  cards = [
+    {
+      title: 'Neat Tree',
+      content: 'Saws this awesome tree during my hike today.',
+      imageUrl: 'assets/tree.jpg',
+      username: 'nature'
+    },
+    {
+      title: 'Huge mountain',
+      content: 'Here is a picture of a huge mountain',
+      imageUrl: 'assets/mountain.jpg',
+      username: 'mountainlover'
+    },
+    {
+      title: 'Hiking',
+      content: 'That was is a very cool hiking today.',
+      imageUrl: 'assets/hiking.jpg',
+      username: 'nature'
+    }
+  ]
 }
